1ca1b2a4063886e834d92ec147b4e4cb4afc64ee
Sagarnil Das <sagarnildass@gmail.com>
2023-02-12 21:57:58 +0530

    changed readme

--- 4_complete_react_project/src/components/Users/AddUser.js
+++ 4_complete_react_project/src/components/Users/AddUser.js
@@ -44,7 +44,13 @@ const AddUser = (props) => {
 
   return (
     <div>
-      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # My codes for the course "React - The complete Guide"
+
+Go to any folder and run:
+    - npm install
+    - npm start
