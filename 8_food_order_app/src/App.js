e6ee7f01696399f27c0f755b97911413f8d0d856
Sagarnil Das <sagarnildass@gmail.com>
2023-02-15 21:14:25 +0530

    completed food order app

--- 8_food_order_app/src/App.js
+++ 8_food_order_app/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
